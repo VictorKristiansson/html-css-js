14f8dc1dfecff5710ec1a2c69cbdb9145e80f6c6
Victor Kristiansson <victorkristiansson123@gmail.com>
2026-08-31 07:17:26 +0200

    add change color page

---
+++ script.js
@@ -0,0 +1,10 @@
+function bytFarg() {
+    if (document.body.style.backgroundColor === "white") {
+        document.body.style.backgroundColor = "red";
+    }
+
+    else {
+        document.body.style.backgroundColor = "white";
+    }
+    
+}
